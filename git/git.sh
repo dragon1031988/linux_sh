bcd179e737b27458eaa7aff6eb64d2b891e6cf73
David <dragon1031988@gmail.com>
2017-04-22 10:59:28 +0700

    change structure source

--- git/git.sh
+++ git/git.sh
@@ -5,6 +5,9 @@ yum install colordiff
 #Copy config git to root
 cp .gitconfig ~/.gitconfig
 
+# Config user
+git config --global user.name "David"
+git config --global user.email dragon1031988@gmail.com
 #Save password 
 git config credential.helper store
 
